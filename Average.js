dc7e446abc996e52e85ceebaa5fc0ff77f349960
rannybears <143052256+rannybears@users.noreply.github.com>
2024-11-06 21:38:52 +0800

    Add files via upload

---
+++ Average.js
@@ -0,0 +1,8 @@
+function calculateAverage() {
+    let x = parseInt(prompt("Enter x:"));
+    let y = parseInt(prompt("Enter y:"));
+    let average = (x + y) / 2;
+    console.log("Average: " + average.toFixed(2));
+}
+
+calculateAverage();
